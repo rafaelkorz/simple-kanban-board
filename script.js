2bdfb89a6d36630044a024fbd7f359402d183b39
Rafael Korz <rafael.korz@gmail.com>
2020-06-13 16:31:04 -0300

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const cards = document.querySelector('.card')
-const dropzones = document.querySelector('.dropzone')
+/** help */
+function log(message) {
+  console.log('> ' + message)
+}
 
+/** app */
+const cards = document.querySelectorAll('.card')
+const dropzones = document.querySelectorAll('.dropzone')
+
+
+/** our cards */
 cards.forEach(card => {
   card.addEventListener('dragstart', dragstart)
   card.addEventListener('drag', drag)
@@ -8,22 +16,27 @@ cards.forEach(card => {
 })
 
 function dragstart() {
-  dropzones.forEach(dropzone => dropzone.classList.add('highlight'))
+  // log('CARD: Start dragging ')
+  dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
 
+  // this = card
   this.classList.add('is-dragging')
 }
 
 function drag() {
-
+  // log('CARD: Is dragging ')
 }
 
 function dragend() {
-  dropzones.forEach(dropzone => dropzone.classList.remove('highlight'))
+  // log('CARD: Stop drag! ')
+  dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
 
+  // this = card
   this.classList.remove('is-dragging')
 }
 
-dropzones.forEach(dropzone => {
+/** place where we will drop cards */
+dropzones.forEach( dropzone => {
   dropzone.addEventListener('dragenter', dragenter)
   dropzone.addEventListener('dragover', dragover)
   dropzone.addEventListener('dragleave', dragleave)
@@ -31,21 +44,28 @@ dropzones.forEach(dropzone => {
 })
 
 function dragenter() {
-
+  // log('DROPZONE: Enter in zone ')
 }
 
 function dragover() {
+  // this = dropzone
   this.classList.add('over')
 
+  // get dragging card
   const cardBeingDragged = document.querySelector('.is-dragging')
 
+  // this = dropzone
   this.appendChild(cardBeingDragged)
 }
 
 function dragleave() {
+  // log('DROPZONE: Leave ')
+  // this = dropzone
   this.classList.remove('over')
+
 }
 
 function drop() {
+  // log('DROPZONE: dropped ')
   this.classList.remove('over')
 }
